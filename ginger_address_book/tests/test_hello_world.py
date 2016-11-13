1019a1999eb9abefb009c99f7a453913f2061aaa
Najeeb Thalakkatt <najeeb.thalakkatt@iquanti.com>
2016-11-14 00:17:31 +0530

    Adding comments

---
+++ ginger_address_book/tests/test_hello_world.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+
+# from ginger_address_book.views import HelloWord
+
+
+class HelloWorldTest(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        # self.obj = HelloWord()
+
+    def tearDown(self):
+        # del self.obj
+        del self.factory
+
+    def test_hello_api(self):
+        expected_output = {'Hello': 'World'}
+        # view= HelloWord.as_view()
+        request = self.factory.get('/hello_world/')
+        # actual_output = view(request)
+        self.assertDictEqual(expected_output, "")
